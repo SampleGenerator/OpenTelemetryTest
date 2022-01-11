6a266e94926b944aa0d434415a41936f6fdad315
fgoodarzi.pr <fgoodarzi.pr@gmail.com>
2022-01-12 01:12:49 +0400

    Add initial projects

---
+++ Metric/Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using System.Diagnostics.Metrics;
+
+Meter MyMeter = new("MyCompany.MyProduct.MyLibrary", "1.0");
+Counter<long> MyFruitCounter = MyMeter.CreateCounter<long>("MyFruitCounter");
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .AddMeter("MyCompany.MyProduct.MyLibrary")
+    .AddConsoleExporter()
+    .Build();
+
+MyFruitCounter.Add(1, new("name", "apple"), new("color", "red"));
+MyFruitCounter.Add(2, new("name", "lemon"), new("color", "yellow"));
+MyFruitCounter.Add(1, new("name", "lemon"), new("color", "yellow"));
+MyFruitCounter.Add(2, new("name", "apple"), new("color", "green"));
+MyFruitCounter.Add(5, new("name", "apple"), new("color", "red"));
+MyFruitCounter.Add(4, new("name", "lemon"), new("color", "yellow"));
---
+++ Trace/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenTelemetry;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
+
+ActivitySource activitySource = new("MyCompany.MyProduct.MyLibrary");
+
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetSampler(new AlwaysOnSampler())
+            .AddSource("MyCompany.MyProduct.MyLibrary")
+            .AddConsoleExporter()
+            .Build();
+
+using var activity = activitySource.StartActivity("SayHello");
+activity?.SetTag("foo", 1);
+activity?.SetTag("bar", "Hello, World!");
+activity?.SetTag("baz", new int[] { 1, 2, 3 });
+activity?.Stop();
